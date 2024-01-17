14c5f9ac6bdea53e5c1459ed0f1c4d8a610592db
Paweł Benkowski <pawlo97.pb@gmail.com>
2024-01-17 17:51:55 +0100

    Testing in supertest and jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,95 @@
+const request = require("supertest")
+const app = require("../../app")
+
+describe("Test GET /launches", () => {
+    it("should respond with 200 success", async () => {
+        const response = await request(app).get("/launches");
+        expect(response.statusCode).toBe(200);
+    })
+})
+
+describe("Test POST /launch", () => {
+    const completeLaunchData = {
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D",
+        destination: "Kepler-186 f",
+        launchDate: "January 4, 2028"
+    }
+
+    it("should respond with 201 created", async () => {
+        const response = await request(app)
+            .post("/launches")
+            .send(completeLaunchData)
+            .expect('Content-Type', /json/)
+            .expect(201)
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+        expect(requestDate).toBe(responseDate)
+
+        const {launchDate, ...rest} = completeLaunchData
+        const launchNoDate = rest
+        expect(response.body).toMatchObject(launchNoDate)
+    })
+
+    it("should catch missing required properties", async () => {
+        const { launchDate, mission, destination, rocket} = completeLaunchData;
+
+        // No mission
+        await request(app)
+            .post("/launches")
+            .send({
+                launchDate,
+                destination,
+                rocket
+            })
+            .expect("Content-Type", /json/)
+            .expect(400)
+        
+        // No destination
+        await request(app)
+            .post("/launches")
+            .send({
+                launchDate,
+                rocket,
+                mission,
+            })
+            .expect("Content-Type", /json/)
+            .expect(400)
+
+        // No rocket
+        await request(app)
+            .post("/launches")
+            .send({
+                launchDate,
+                mission,
+                destination
+            })
+            .expect("Content-Type", /json/)
+            .expect(400)
+
+        // No launch date
+        await request(app)
+            .post("/launches")
+            .send({
+                rocket,
+                mission,
+                destination
+            })
+            .expect("Content-Type", /json/)
+            .expect(400)
+    })
+
+    it("should catch invalid dates", async () => {
+        const { launchDate, ...rest} = completeLaunchData;
+
+        const resNoLaunchDate = await request(app)
+            .post("/launches")
+            .send({
+                ...rest,
+                launchDate: "Wrong date"
+            })
+            .expect("Content-Type", /json/)
+            .expect(400)
+    })
+})
